af69c201df3068a3ccda590ba1b146f3894ee9f9
Ranga Reddy <rangareddy.avula@gmail.com>
2021-08-21 14:09:42 +0530

    Update KafkaSecureProducerDemo.java

--- kafka-java-api/src/main/java/com/ranga/kafka/security/KafkaSecureProducerDemo.java
+++ kafka-java-api/src/main/java/com/ranga/kafka/security/KafkaSecureProducerDemo.java
@@ -45,7 +45,7 @@ public class KafkaSecureProducerDemo {
         Producer<String, String> kafkaProducer = null;
         try {
             kafkaProducer = new KafkaProducer<>(props);
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 1000; i++) {
                 ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, Integer.toString(i), "test message - " + i);
                 kafkaProducer.send(producerRecord, new Callback() {
                     @Override
